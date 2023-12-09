d6233be7801cd94804f63ae3b3b017c3204dfe4c
Kynedi <Kynedi.Paulson@trojans.dsu.edu>
2023-12-09 10:28:22 -0700

    Created End of game selection button

---
+++ ChessLogic/Result.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Result
+    {
+        public Player Winner { get; }
+        public Ending Reason { get; }
+
+        public Result(Player winner, Ending reason)
+        {
+            Winner = winner;
+            Reason = reason;
+        }
+
+        //Types of ending for the game...Checkmate and Draw
+        public static Result Win (Player winner)
+        {
+            return new Result(winner, Ending.Checkmate);
+        }
+        public static Result Draw (Ending reason)
+        {
+            return new Result(Player.none, reason);
+        }
+
+    }
+}
--- ChessLogic/Status.cs
+++ ChessLogic/Status.cs
@@ -10,6 +10,7 @@ namespace ChessLogic
     {
         public ChessBoard Board { get; }
         public Player CPlayer { get; private set; }
+        public Result result { get; private set; } = null;
 
         public Status(Player player, ChessBoard board)
         {
@@ -32,6 +33,38 @@ namespace ChessLogic
         {
             move.Excecute(Board);
             CPlayer = CPlayer.Opponent();
+            CheckForEnd();
+        }
+
+        public IEnumerable<Move> AllLegalPlayerMoves(Player player)
+        {
+            IEnumerable<Move> moveCandidates = Board.PositionForColor(player).SelectMany(pos =>
+            {
+                Piece piece = Board[pos];
+                return piece.getMoves(pos, Board);
+            });
+
+            return moveCandidates.Where(move => move.MoveIsLegal(Board));
+        }
+
+        private void CheckForEnd()
+        {
+            if (!AllLegalPlayerMoves(CPlayer).Any())
+            {
+                if (Board.IsInCheck(CPlayer))
+                {
+                    result = Result.Win(CPlayer.Opponent()); 
+                }
+                else
+                {
+                    result = Result.Draw(Ending.Stalemate);
+                }
+            }
+        }
+
+        public bool IsOver()
+        {
+            return result != null;
         }
     }
 }
